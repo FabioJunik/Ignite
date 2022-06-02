cbcdb1e03e7d551b89e1f3fe654a859112e0630e
Fábio Junik <fabiojunik@gmail.com>
2022-06-02 11:09:19 +0100

    Estrutura inicial

---
+++ Desafio_I-- Conseitos_do_ React/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import TaskList from './Components/TaskList';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <TaskList/>
+    </div>
+  );
+}
+
+export default App;
