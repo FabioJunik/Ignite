7a6221900e269375777cfb83c53e962739ecf6e9
Fábio Junik <fabiojunik@gmail.com>
2022-06-03 22:01:52 +0100

    criando as tasks

--- Desafio_I--Conseitos_do_React/src/Components/TaskList/index.tsx
+++ Desafio_I--Conseitos_do_React/src/Components/TaskList/index.tsx
@@ -1,15 +1,47 @@
-import React  from 'react'
+import React , {useState} from 'react'
 import './styles.scss';
-import {FiCheckSquare } from 'react-icons/fi';
+import {FiCheckSquare, FiTrash } from 'react-icons/fi';
+
+interface Task{
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
 
 const TaskList = () => {
+
+  const [title, setTitle] = useState<string>('');
+  const [tasks,setTasks] = useState<Task[]>([]);
+
+  function createNewTask(title:string){
+    const data={
+      id: 12,
+      title: title,
+      isComplete: true,
+    }
+    setTasks([...tasks, data]);
+  }
   return (
     <div className='taskList'>
       <h2>Minhas tasks</h2>
-      <form>
-        <input type="text" placeholder='Adicionar novo todo'/>
-        <button><FiCheckSquare/></button>
+      <form onSubmit={(event)=>{event.preventDefault()}}>
+        <input type="text" placeholder='Adicionar novo todo' 
+          onChange={({target})=> setTitle(target.value)}
+        />
+        <button onClick={()=>createNewTask(title)}><FiCheckSquare/></button>
       </form>
+      <div className='tasks'>
+        {tasks.map(task=>(
+          <p key={task.id}>
+            <span>
+              <input type="checkbox" />
+              <p>{task.title}</p>
+            </span>
+            <FiTrash/>
+        </p>
+        ))}
+      </div>
+      
     </div>
   )
 }
