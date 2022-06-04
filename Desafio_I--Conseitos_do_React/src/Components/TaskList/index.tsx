00cf3b40f4185eb08d5ecf37637391e696a7973e
Fábio Junik <fabiojunik@gmail.com>
2022-06-04 06:55:09 +0100

    Setando e buscando os valores do localStorege

--- Desafio_I--Conseitos_do_React/src/Components/TaskList/index.tsx
+++ Desafio_I--Conseitos_do_React/src/Components/TaskList/index.tsx
@@ -1,4 +1,4 @@
-import React , {useState} from 'react'
+import React , {useEffect, useState} from 'react'
 import './styles.scss';
 import {FiCheckSquare, FiTrash } from 'react-icons/fi';
 
@@ -12,29 +12,43 @@ const TaskList = () => {
 
   const [title, setTitle] = useState<string>('');
   const [tasks,setTasks] = useState<Task[]>([]);
+  
+  useEffect(()=>{
+    if(localStorage.getItem('tasks'))
+    {
+      const localTasks = JSON.parse(localStorage.getItem('tasks') || '');
+      setTasks(localTasks);
+    }
+  },[]);
 
-  function createNewTask(title:string){
+  function createNewTask(){
     const data={
-      id: 12,
+      id: tasks.length,
       title: title,
-      isComplete: true,
+      isComplete: false,
     }
     setTasks([...tasks, data]);
+    localStorage.setItem('tasks',JSON.stringify([...tasks,data]));
   }
+
   return (
-    <div className='taskList'>
+    <section className='taskList'>
       <h2>Minhas tasks</h2>
       <form onSubmit={(event)=>{event.preventDefault()}}>
-        <input type="text" placeholder='Adicionar novo todo' 
+        <input 
+          type="text" 
+          placeholder='Adicionar novo todo' 
           onChange={({target})=> setTitle(target.value)}
         />
-        <button onClick={()=>createNewTask(title)}><FiCheckSquare/></button>
+        <button onClick={createNewTask}><FiCheckSquare/></button>
       </form>
       <div className='tasks'>
         {tasks.map(task=>(
           <p key={task.id}>
             <span>
-              <input type="checkbox" />
+              <input type="checkbox" 
+                checked={task.isComplete} 
+              />
               <p>{task.title}</p>
             </span>
             <FiTrash/>
@@ -42,7 +56,7 @@ const TaskList = () => {
         ))}
       </div>
       
-    </div>
+    </section>
   )
 }
 
